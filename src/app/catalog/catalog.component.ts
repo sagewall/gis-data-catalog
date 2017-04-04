3315b87e3adb21911582a0c9d32f81f857688c89
sagewall <sagemaps@gmail.com>
2017-04-04 15:46:03 -0600

    Add tag filtering

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Dataset } from '../dataset';
 import { DatasetService } from '../dataset.service';
+import { DatasetSearchService } from '../dataset-search.service';
 import { TagService } from '../tag.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.sass']
+  styleUrls: ['./catalog.component.sass'],
+  providers: [DatasetSearchService]
 })
 export class CatalogComponent implements OnInit {
 
   datasets: Dataset[];
   tags: string[];
+  selectedTag: string;
 
   constructor(
     private datasetService: DatasetService,
+    private datasetSearchService: DatasetSearchService,
     private tagService: TagService
   ) { }
 
@@ -28,4 +32,17 @@ export class CatalogComponent implements OnInit {
       .subscribe(tags => this.tags = tags);
   }
 
+  filterDatasets(tag) {
+    if (tag !== this.selectedTag) {
+      this.selectedTag = tag;
+      this.datasetSearchService
+        .search(tag)
+        .subscribe(datasets => this.datasets = datasets);
+    } else {
+      this.selectedTag = null;
+      this.datasetService
+        .getDatasets()
+        .subscribe(datasets => this.datasets = datasets);
+    }
+  }
 }
